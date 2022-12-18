d8a344fabbc06e39934d84171b6abc469c3dddd4
coding-to-music <connors.tom@gmail.com>
2022-12-18 06:39:59 +0000

    Build

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,28 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase-key.json");
+// var serviceAccount = require("./firebase-key.json");
+
+// apiKey: process.env.FIREBASE_API_KEY,
+// authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+// projectId: process.env.FIREBASE_PROJECT_ID,
+// storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+// messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+// appId: process.env.FIREBASE_APP_ID,
+// measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
+
+// console.log(serviceAccount); // looks good
 
 admin.initializeApp({
   credential: admin.credential.cert({
